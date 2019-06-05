15c18a63523c2b2f7034aae1a29005b7330e5b24
hiimdat95 <letiendat2808@gmail.com>
2019-06-05 17:37:53 +0700

    Add router

---
+++ liyobe-client/src/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './example';
+import 'assets/css/vendor/bootstrap.min.css';
+import 'assets/css/sass/themes/gogo.light.blue.scss';
+import 'assets/fonts/simple-line-icons/css/simple-line-icons.css';
+import 'assets/fonts/iconsmind/style.css';
+
+
+ReactDOM.render(
+<App />,
+document.getElementById('root'));
